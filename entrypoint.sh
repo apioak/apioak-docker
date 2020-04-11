b63522be8435c52f455374c22ae29ede549533ed
hujian <516870171@qq.com>
2020-04-11 17:58:11 +0800

    docker-compose初次提交

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sed -i "s@127.0.0.1@$DB_HOST@g" /usr/local/apioak/conf/apioak.yaml
+sed -i "s@3306@$DB_PORT@g" /usr/local/apioak/conf/apioak.yaml
+sed -i "s@apioak@$DB_NAME@g" /usr/local/apioak/conf/apioak.yaml
+sed -i "s@root@$DB_USER@g" /usr/local/apioak/conf/apioak.yaml
+sed -i "s@123000@$DB_PASSWORD@g" /usr/local/apioak/conf/apioak.yaml
+
+sed -i "s@resolver 8.8.8.8 114.114.114.114 ipv6=off;@resolver $(grep nameserver /etc/resolv.conf|awk '{printf $2" "}')ipv6=off;@g"  /usr/local/apioak/conf/nginx.conf
+
+
+if [[ -z $(mysql -u$DB_USER -p$DB_PASSWORD -h$DB_HOST -P$DB_PORT $DB_NAME -e "show tables;" |grep "oak_users") ]];then
+    mysql -u$DB_USER -p$DB_PASSWORD -h$DB_HOST -P$DB_PORT $DB_NAME < /usr/local/apioak/conf/apioak.sql
+fi
+
+
+/usr/bin/apioak start
