a7fe3a93564b5d462371f3b0a58c67f432cce0fa
hujian <32188410+hryGithub@users.noreply.github.com>
2020-04-13 14:42:36 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ sed -i "s@123000@$DB_PASSWORD@g" /usr/local/apioak/conf/apioak.yaml
 
 sed -i "s@resolver 8.8.8.8 114.114.114.114 ipv6=off;@resolver $(grep nameserver /etc/resolv.conf|awk '{printf $2" "}')ipv6=off;@g"  /usr/local/apioak/conf/nginx.conf
 
-#init db
+#init_db
 if [[ -z $(mysql -u$DB_USER -p$DB_PASSWORD -h$DB_HOST -P$DB_PORT $DB_NAME -e "show tables;" |grep "oak_users") ]];then
     mysql -u$DB_USER -p$DB_PASSWORD -h$DB_HOST -P$DB_PORT $DB_NAME < /usr/local/apioak/conf/apioak.sql
 fi
